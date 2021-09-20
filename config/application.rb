7dc7fd0cb7a14b8369a70fb9c072f5a19da983c0
akinari <a.mizo0919@gmail.com>
2021-09-20 14:00:12 +0900

    rails g作成ファイルの制限&TOPページルーティング反映確認

--- config/application.rb
+++ config/application.rb
@@ -2,17 +2,18 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Rubizan
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # assets、helper、testファイル、ルーティングが生成されないように設定
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
